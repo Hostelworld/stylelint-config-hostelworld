11716cf848917ca49d103a178f5064d45a21c3e4
Ignacio Velazquez <ignacio.gomez@hostelworld.com>
2017-10-31 15:50:37 +0000

    Added scss plugin

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+const kebabCase = '([a-z][a-z0-9]*(-[a-z0-9]+)*)'; // kebab-case
+
 module.exports = {
     'extends': [
         'stylelint-config-standard'
     ],
+    plugins: [
+        'stylelint-scss'
+    ],
     'rules': {
-        'indentation': 4
+        'indentation': 4,
+        'selector-class-pattern': [`^${kebabCase}$`, {
+            resolveNestedSelectors: true,
+            message: 'Expected class selector in kebab-case naming format (selector-class-pattern)'
+        }],
+        'scss/dollar-variable-pattern': [`^${kebabCase}$`, {
+            message: 'Expected $ variable to be kebab-case (scss/dollar-variable-pattern)'
+        }]
     }
 };
--- test/index.js
+++ test/index.js
@@ -1,11 +1,19 @@
 /* global describe, it, beforeEach */
 import fs from 'fs';
 import stylelint from 'stylelint';
-import { expect } from 'chai';
 import config from './..';
+import chai from 'chai';
+import chaiThings from 'chai-things';
+import chaiLike from 'chai-like';
 
-const validCss = fs.readFileSync('./test/css-valid.css', 'utf-8');
-const invalidCss = fs.readFileSync('./test/css-invalid.css', 'utf-8');
+chai.should();
+chai.use(chaiLike);
+chai.use(chaiThings);
+
+const expect = chai.expect;
+
+const validCss = fs.readFileSync('./test/css-valid.scss', 'utf-8');
+const invalidCss = fs.readFileSync('./test/css-invalid.scss', 'utf-8');
 
 describe('flags no warnings with valid css', () => {
     let result;
@@ -46,25 +54,29 @@ describe('flags warnings with invalid css', () => {
         ));
     });
 
-    it('flags three errors', () => {
+    it('flags four errors', () => {
         return result.then(data => (
-            expect(data.results[0].warnings.length).to.equal(3)
+            expect(data.results[0].warnings.length).to.equal(5)
         ));
     });
 
     it('correct warning text', () => {
         return result.then(data => {
-            expect(data.results[0].warnings[0].text).to.equal('Expected indentation of 4 spaces (indentation)');
-            expect(data.results[0].warnings[1].text).to.equal('Expected a leading zero (number-leading-zero)');
-            expect(data.results[0].warnings[2].text).to.equal('Expected newline after "," (selector-list-comma-newline-after)');
+            expect(data.results[0].warnings).to.be.an('array').that.contains.something.like({ text: 'Expected indentation of 4 spaces (indentation)' });
+            expect(data.results[0].warnings).to.be.an('array').that.contains.something.like({ text: 'Expected class selector in kebab-case naming format (selector-class-pattern)' });
+            expect(data.results[0].warnings).to.be.an('array').that.contains.something.like({ text: 'Expected a leading zero (number-leading-zero)' });
+            expect(data.results[0].warnings).to.be.an('array').that.contains.something.like({ text: 'Expected newline after "," (selector-list-comma-newline-after)' });
+            expect(data.results[0].warnings).to.be.an('array').that.contains.something.like({ text: 'Expected $ variable to be kebab-case (scss/dollar-variable-pattern)' });
         });
     });
 
     it('correct rule flagged', () => {
         return result.then(data => {
-            expect(data.results[0].warnings[0].rule).to.equal('indentation');
-            expect(data.results[0].warnings[1].rule).to.equal('number-leading-zero');
-            expect(data.results[0].warnings[2].rule).to.equal('selector-list-comma-newline-after');
+            expect(data.results[0].warnings).to.be.an('array').that.contains.something.like({ rule: 'indentation' });
+            expect(data.results[0].warnings).to.be.an('array').that.contains.something.like({ rule: 'selector-class-pattern' });
+            expect(data.results[0].warnings).to.be.an('array').that.contains.something.like({ rule: 'number-leading-zero' });
+            expect(data.results[0].warnings).to.be.an('array').that.contains.something.like({ rule: 'selector-list-comma-newline-after' });
+            expect(data.results[0].warnings).to.be.an('array').that.contains.something.like({ rule: 'scss/dollar-variable-pattern' });
         });
     });
 
@@ -73,22 +85,7 @@ describe('flags warnings with invalid css', () => {
             expect(data.results[0].warnings[0].severity).to.equal('error');
             expect(data.results[0].warnings[1].severity).to.equal('error');
             expect(data.results[0].warnings[2].severity).to.equal('error');
-        });
-    });
-
-    it('correct line number', () => {
-        return result.then(data => {
-            expect(data.results[0].warnings[0].line).to.equal(2);
-            expect(data.results[0].warnings[1].line).to.equal(2);
-            expect(data.results[0].warnings[2].line).to.equal(1);
-        });
-    });
-
-    it('correct column number', () => {
-        return result.then(data => {
-            expect(data.results[0].warnings[0].column).to.equal(3);
-            expect(data.results[0].warnings[1].column).to.equal(8);
-            expect(data.results[0].warnings[2].column).to.equal(2);
+            expect(data.results[0].warnings[3].severity).to.equal('error');
         });
     });
 });
